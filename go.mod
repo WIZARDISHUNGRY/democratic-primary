ab36390be2aa6b4ee08ff9039e0453af648b02d3
Jon Williams <jon@jonwillia.ms>
2019-03-16 12:18:46 -0400

    initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module vote
+
+go 1.12
+
+require (
+	github.com/Sam-Izdat/govote v0.0.2
+	github.com/stretchr/testify v1.3.0
+)
---
+++ vote_test.go
@@ -0,0 +1,156 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"runtime"
+	"testing"
+
+	"github.com/Sam-Izdat/govote"
+	"github.com/stretchr/testify/require"
+)
+
+func democrats() map[string]int {
+	return map[string]int{
+		"Biden":        29,
+		"Sanders":      20,
+		"Harris":       5,
+		"O'Rourke":     4,
+		"Warren":       4,
+		"Booker":       3,
+		"Delaney":      3,
+		"Klobuchar":    3,
+		"Castro":       1,
+		"Gabbard":      1,
+		"Gillibrand":   1,
+		"Hickenlooper": 1,
+		"Inslee":       1,
+		"Yang":         1,
+		"Buttigieg":    0,
+		"Williamson":   0,
+	}
+}
+
+func candidateList(weights map[string]int) []string {
+	out := make([]string, 0, len(weights))
+	for name := range weights {
+		out = append(out, name)
+	}
+	return out
+}
+
+func weighted(weights map[string]int) []*string {
+	i := 0
+	length := 100 + len(weights)
+	result := make([]*string, 0, length)
+	for name, v := range weights {
+	REDO:
+		n := name // BUG: loop capture?
+		fmt.Printf("adding %s at %d\n", name, i)
+		result = append(result, &n)
+		i++
+		v--
+		if v > 0 {
+			goto REDO
+		}
+	}
+	return result
+}
+
+func randomBallot(random *rand.Rand, choices []*string, min, max int) []string {
+	if min > max {
+		panic("min > max")
+	}
+	if min < 1 {
+		panic("min < 1")
+	}
+	if max > len(choices) {
+		panic("max > len(choices)")
+	}
+
+	count := rand.Intn(max-min) + min
+	votes := make(map[*string]bool, count)
+	output := make([]string, count, count)
+	for ; count > 0; count-- {
+	RETRY: // suboptimal but better than building a giant decision tree in memory since we don't expect more than a handful of votes per
+		index := random.Intn(len(choices))
+		vote := choices[index]
+		if exists := votes[vote]; exists {
+			// fmt.Printf("exists %d %s\n", index, *vote)
+			goto RETRY
+		}
+		// fmt.Printf("ranked %d %s\n", index, *vote)
+		votes[vote] = true
+		output[count-1] = *vote
+	}
+	return output
+}
+
+func TestVeryMany(t *testing.T) {
+
+	random := rand.New(rand.NewSource(2019))
+
+	var a string = "Hello"
+	var b string = "Hello"
+
+	const (
+		population = 328285992
+		buffer     = 1 << 16
+	)
+
+	require.Equal(t, a, b, "The two words should be the same.")
+	dems := democrats()
+	candidates := candidateList(dems)
+	deck := weighted(dems)
+	fmt.Println(candidates)
+	poll, _ := govote.Schulze.New(candidates)
+	// poll.AddBallot([]string{"Kang"})
+	// poll.AddBallot([]string{"Kang"})
+	// poll.AddBallot([]string{"Kodos"})
+
+	c := make(chan []string)
+	length := runtime.GOMAXPROCS(0)
+	quitters := make([]chan bool, length)
+	for i := 0; i < length; i++ {
+		quitters[i] = make(chan bool)
+		s := rand.NewSource(random.Int63())
+		random := rand.New(s)
+		go func(quit chan bool) {
+		RETRY:
+			ballot := randomBallot(random, deck, 1, len(candidates))
+			select {
+			case <-quit:
+				//fmt.Println("voter quitting")
+			case c <- ballot:
+				goto RETRY
+			}
+		}(quitters[i])
+	}
+
+	for i := 0; i < population; i++ {
+		ballot := <-c
+		if i%100000 == 0 {
+			fmt.Println(i, ballot)
+		}
+		poll.AddBallot(ballot)
+	}
+	for i := 0; i < length; i++ {
+		quitters[i] <- true
+	}
+	close(c)
+	fmt.Println("calculating")
+	result, scores, err := poll.Evaluate()
+	if err != err {
+		panic(err)
+	}
+	s := "no result"
+	if len(result) > 1 {
+		s = "draw"
+	} else if len(result) == 1 {
+		s = "winner"
+	}
+	fmt.Println(s, " ", result)
+	for i, score := range scores {
+		fmt.Printf("% 3.d % 29s\t% 4d\n", i+1, score.Name, score.Score)
+	}
+}
